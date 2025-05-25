869b70d4ed8df6880f9bc633ca6491397a083495
hermella wondwosen <hermiwy>
2025-05-25 05:31:42 -0700

    database working with sqlworkshop8.0

--- onlineSchool/onlineSchool/settings.py
+++ onlineSchool/onlineSchool/settings.py
@@ -78,12 +78,24 @@ WSGI_APPLICATION = 'onlineSchool.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+    
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'onlinetutorschool',
+        'USER':'root',
+        'PASSWORD':'NewPassword123!',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
+
 
 
 # Password validation
